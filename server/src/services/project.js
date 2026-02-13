99d0a587f577ffa7c52809c57604c8979604dbc4
Sajal234 <banwalasajal.work@gmail.com>
2026-02-13 11:53:34 +0530

    feat(project.js): added validation for projectId before DB query

--- server/src/services/project.js
+++ server/src/services/project.js
@@ -1,9 +1,15 @@
 import { Project } from "../models/Project.js";
 import { ApiError } from "../utils/ApiError.js";
+import mongoose from "mongoose";
 
 
 // get project by id or throw error
 export const getProjectByIdOrThrow = async(projectId) =>{
+
+    if(!mongoose.Types.ObjectId.isValid(projectId)){
+        throw new ApiError(400, "Invalid project ID");
+    }
+    
     const project = await Project.findById(projectId);
 
     if(!project){

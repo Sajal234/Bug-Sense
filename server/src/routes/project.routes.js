27270431178b4c980b43a8336df0825e542a7c5b
Sajal234 <banwalasajal.work@gmail.com>
2026-02-23 01:32:44 +0530

    feat(routes): add reopen workflow routes and fix route definitions
    - Add PATCH /bugs/:bugId/request-reopen route
    - Add PATCH /bugs/:bugId/approve-reopen route
    - Change submitFix from PATCH to POST (creates new BugFix resource)
    - Remove trailing slash from bug detail route

--- server/src/routes/project.routes.js
+++ server/src/routes/project.routes.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import { createProject, getMyProjects, joinProject } from "../controllers/project.controller.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
-import { approveBug, assignBug, createBug, getBugInfo, getProjectBugs, rejectBug, requestReopen, submitFix } from "../controllers/bug.controller.js";
+import {
+    approveBug, approveReopen,
+    assignBug, createBug, getBugInfo,
+    getProjectBugs, rejectBug, requestReopen,
+    submitFix 
+} from "../controllers/bug.controller.js";
 import rateLimit from 'express-rate-limit';
 
 
@@ -36,16 +41,19 @@ router.route("/:projectId/bugs/:bugId/approve")
 router.route("/:projectId/bugs/:bugId/reject")
 .patch(generalLimiter, verifyJWT, rejectBug);
 
-router.route("/:projectId/bugs/:bugId/")
+router.route("/:projectId/bugs/:bugId")
 .get(generalLimiter, verifyJWT, getBugInfo);
 
 router.route("/:projectId/bugs/:bugId/assign")
 .patch(generalLimiter, verifyJWT, assignBug);
 
 router.route("/:projectId/bugs/:bugId/fix")
-.patch(generalLimiter, verifyJWT, submitFix);
+.post(generalLimiter, verifyJWT, submitFix);
 
-router.route("/:projectId/bugs/:bugId/reopen")
+router.route("/:projectId/bugs/:bugId/request-reopen")
 .patch(generalLimiter, verifyJWT, requestReopen);
+
+router.route("/:projectId/bugs/:bugId/approve-reopen")
+.patch(generalLimiter, verifyJWT, approveReopen);
 
 export default router;

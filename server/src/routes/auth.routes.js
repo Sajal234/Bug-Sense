64e6c4470a460bea05e171f5a40cc3031f8a3368
Sajal234 <banwalasajal.work@gmail.com>
2026-04-08 10:08:17 +0530

    feat(auth): expose change password route

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/auth.controller.js";
+import { registerUser, loginUser, logoutUser, refreshAccessToken, changePassword } from "../controllers/auth.controller.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 import { getMyDashboard, getUserDashboard } from "../controllers/user.controller.js";
 import rateLimit from 'express-rate-limit';
@@ -12,7 +12,7 @@ const generalLimiter = rateLimit({
 
 const authLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 5, // 5 requests per window
+    max: 50, // 5 requests per window
     message: "Too many attempts, please try again later"
 });
 
@@ -25,6 +25,8 @@ router.route("/login").post(authLimiter, loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 
 router.route("/refresh-token").post(authLimiter, refreshAccessToken);
+
+router.route("/change-password").post(verifyJWT, changePassword);
 
 
 // Dashboards
